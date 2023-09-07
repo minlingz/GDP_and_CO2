f2db10b9b1767b825b1da56cf764e8f354bd70c2
Ninsta22 <kbagherlee@gmail.com>
2023-09-07 13:56:01 -0400

    Added Comments

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+# These are our import statements
 import pandas as pd
 import matplotlib
 
+# This opens the csv document and saves it
 df = pd.read_csv(
     "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
 )
 
+# This actually plots the requested information
 df[
     [
         "Mortality rate, infant (per 1,000 live births)",
