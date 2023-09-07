542783513d514dc84c0c8b81b0e246b2c0e4f1a2
Ninsta22 <kbagherlee@gmail.com>
2023-09-07 13:49:13 -0400

    Analysis File

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-import
+import pandas as pd
+import matplotlib
+
+df = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+
+df[
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+    ]
+].plot()
